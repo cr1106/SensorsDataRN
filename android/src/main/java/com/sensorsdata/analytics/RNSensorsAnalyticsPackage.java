4da05262e0f63980313a4bd0d18bca8043cb7008
chenru <chenru@sensorsdata.cn>
2020-04-29 11:46:11 +0800

    no message

--- android/src/main/java/com/sensorsdata/analytics/RNSensorsAnalyticsPackage.java
+++ android/src/main/java/com/sensorsdata/analytics/RNSensorsAnalyticsPackage.java
@@ -15,6 +15,7 @@ import java.util.List;
  */
 
 public class RNSensorsAnalyticsPackage implements ReactPackage {
+    public static final String VERSION = "2.0.0";
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
